fd305c0230138ddf6826cb87eb56b484aa12516b
Zidaan Dutta <zdutta@purdue.edu>
2018-04-14 23:40:01 -0400

    Building up the models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from config import app_config
-from app import views
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+
 
 def create_app(config_name):
 	app = Flask(__name__,instance_relative_config=True)
@@ -11,8 +14,12 @@ def create_app(config_name):
 	app.config.from_pyfile('config.py')
 	db.init_app(app)
 
-	app.route("/")
+	@app.route('/')
 	def hello_world():
 		return 'Hello, World!'
+
+	login_manager.init_app(app)
+	login_manager.login_message="Must be logged in to access this page."
+	login_manage.login_view = "auth.login"
 
 	return app
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'abc1324'
-SQLALCEHMY_DATABASE_URI='mysql://db_amd'
+SQLALCHEMY_DATABASE_URI = 'mysql://db_admin:db2018@localhost/tutorMatcher_db'
